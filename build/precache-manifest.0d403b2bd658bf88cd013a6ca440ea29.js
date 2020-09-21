789a9d51fc9fd1b1ef950722e41d68b049acdd7f
Reda404 <julienmerlin.dev@gmail.com>
2020-09-21 12:57:30 +0200

    Deploy UI

---
+++ build/precache-manifest.0d403b2bd658bf88cd013a6ca440ea29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f5c395dd1be4940aec02ee7cd25d19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06fe5effbacc1a815fa9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2469ec848eef6bb55628",
+    "url": "/static/js/2.90705a08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90705a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06fe5effbacc1a815fa9",
+    "url": "/static/js/main.62de6ceb.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
